8089cada8a20e37aaf5bc71d5b6c2312c612a8a1
Perla Escano <perladeluzes@gmail.com>
2022-03-21 17:39:37 -0400

    Update PrimeNumberGenerator.py
    added docstring

--- PrimeNumberGenerator.py
+++ PrimeNumberGenerator.py
@@ -1,7 +1,14 @@
 # Perla Escano Estrella
 # CSCI 39538
 # Recreational Math Assignment
+""" 
+Prime numbers are whole numbers greater than 1, that have only two factors: 1 and the number itself.
+Prime numbers are divisible only by the number 1 or itself. 
 
+The following code generates all the prime numbers up to the number the user denotes.
+
+
+"""
 n = int(input("Hi, I am a prime number generator!\n Up to what number would you like me to generate the primes? : "))
 
 for x in range(2, n):
